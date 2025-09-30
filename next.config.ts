66983c3d5d3e08e27d5473940195ce296bca4f57
Dhiraj <patildhiraj309@outlook.com>
2025-09-30 18:56:58 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* existing config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // ignore ESLint/TS warnings during build
+  },
 };
 
 export default nextConfig;
